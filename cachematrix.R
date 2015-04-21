eaba522d51ea08ae5a399afa463e26e583377f77
Ramesh <ramesh.gopalakrishna@gmail.com>
2015-04-21 19:05:41 +0530

    few more comments added

--- cachematrix.R
+++ cachematrix.R
@@ -1,24 +1,28 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
+
+## This function creates a matrix object which has the ability to 
+## calculate the inverse of the matrix and cache the inverse 
+## Everytime the matrix changes the inverse of the matrix is 
+## calculated and cached
 
 makeCacheMatrix <- function(my_mat = matrix()) {
   
-  ## initialize my_mat to empty matrix
-  ## inv is the inverse of the my_mat; 
-  ## set inv to NULL at the start
+  ## initialize function variable 'my_mat' to empty matrix
+  ## The variable 'inv' contains the inverse of the my_mat; 
   
+  ## set inv to NULL at the start
   inv <- NULL
   
-  ## This is the set function, use this to assign a matrix
+  ## This is the set function, use this to assign a matrix 
+  ## for which inverse is needed  and 
   ## Set "inv" to NULL as my_mat has changed
   setmatrix <- function(y) {
     my_mat <<- y
     inv <<- NULL
   }
   
-  ## The access function is named 'get'
+  ## The access function is named 'getmatrix'
+  ## This will return the matrix thta was set using setmatrix
   getmatrix <- function() my_mat
   
   ## Assign the inverse of the matrix
@@ -33,20 +37,23 @@ makeCacheMatrix <- function(my_mat = matrix()) {
 
 
 
-## Write a short comment describing this function
+## This function is used to calculate the inverse of the matrix
+## It uses the solve() function to acheive this
 
 cacheSolve <- function(x, ...) {
   ## Return a matrix that is the inverse of 'x'
   
   ## Find out if the cached copy is already presnt.
-  ## If present then return the invers 'inv'
+  ## If present then return the inverse matrix that was already cached 'inv'
   inv <- x$getinverse()
   if(!is.null(inv)) {
     message("getting cached data")
     return(inv)
   }
   
-  ## inverse is not yet calculated, need to do it now. 
+  ## The variable 'inv' is NULL so inverse is not yet calculated. 
+  ## need to caluclate it now using solve(). 
+  
   ## First get the matrix to 'data'
   data <- x$getmatrix()
   inv <- solve(data, ...)   ## use solve to get the inverse of the matrix
